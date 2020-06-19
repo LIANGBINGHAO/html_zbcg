21e08487b0d9113d43fc61ea87f31c8fc0c2f337
LBH <874572545@qq.com>
2020-06-19 18:19:22 +0800

    切图

--- js/index.js
+++ js/index.js
@@ -13,6 +13,27 @@ var Index = {
       var mySwiper = new Swiper(".banner-swiper", {
         autoplay: 5000, //可选选项，自动滑动
         pagination: ".gzdt-pagination",
+        paginationClickable: true,
+      });
+      var left = "";
+      $(".main .notice .head .header-title span").each(function (index) {
+        $(this).click(function () {
+          left = 106 * index + "px";
+          console.log(index, left, "index");
+          $(this).siblings().removeClass("active");
+          $(this).addClass("active");
+          $(this).parent().find(".line").stop().animate(
+            {
+              left: left,
+            },
+            "fast",
+            "linear"
+          );
+          $(this).parent().parent().find("a").siblings().removeClass("active");
+          $(this).parent().parent().find("a").eq(index).addClass("active");
+          $(".main .notice .notice-news .tab").siblings().removeClass("active");
+          $(".main .notice .notice-news .tab").eq(index).addClass("active");
+        });
       });
     };
   },
